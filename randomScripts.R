1e039f6181bf37c9884a5f45cdb3513950cce05a
erichhuang <erich.huang@sagebase.org>
2012-04-13 09:23:35 -0700

    Pushed up to Synapse.

---
+++ randomScripts.R
@@ -0,0 +1,8 @@
+# randomScripts.R
+
+# SAVE CODE ENTITY ONE "GGHEAT2"
+ggheat2Layer <- Code(list(name = "ggheat2.R",
+                                parentId = 274060))
+ggheat2Layer <- createEntity(ggheat2Layer)
+ggheat2Layer <- addFile(ggheat2Layer, "ggheat2.R")
+ggheat2Layer <- storeEntity(ggheat2Layer)
